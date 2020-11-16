290b17d0299a70590342ed8c3dc852855e607dc2
Sourav Patra <joshpatra12@gmail.com>
2020-11-16 10:32:50 -0500

    SwitchPreference
    Working, but not updating

--- app/src/main/java/com/josh/trackcovid19v2/SettingsActivity.java
+++ app/src/main/java/com/josh/trackcovid19v2/SettingsActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -26,6 +29,19 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            SwitchPreference darkModeSwitch = findPreference("darkMode");
+            assert darkModeSwitch != null;
+            darkModeSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    }
+                    return false;
+                }
+            });
         }
     }
 }
